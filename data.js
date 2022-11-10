d6c812a4b127df87075d5868ee80f0b013ec45e5
Raymond <rmach1992@gmail.com>
2022-11-09 22:15:17 -0800

    Adds function to add items and calculate total

--- data.js
+++ data.js
@@ -4,20 +4,23 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushroom", "mozzarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        count: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
+        id: 1,
         price: 12,
         emoji: "🍔",
-        id: 1
+        count: 0
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
+        id: 2,
         price: 12,
         emoji: "🍺",
-        id: 2
+        count: 0
     }
 ]
--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 import { menuArray } from "./data.js";
 
 const mainMenu = document.getElementById('main-menu')
+const order = document.getElementById('order-content')
+const orderedItems = []
 
 document.addEventListener('click', function(e) {
     if(e.target.dataset.id) {
         handleAddClick(e.target.dataset.id)
     }
+    // else if (e.target.id === 'remove-btn'){
+    //     removeItem(e.target.parentNode)
+    // }
 })
 
 function handleAddClick(id){
-    console.log(id)
+    const targetMenuObj = menuArray.filter(function(item){
+        return item.id === parseInt(id)
+    })[0]
+
+    if(orderedItems.includes(targetMenuObj)) {
+        orderedItems.forEach(function(item){
+            if(item.id === targetMenuObj.id) {
+                item.count++
+            }
+        })
+    }
+    else {
+        targetMenuObj.count++
+        orderedItems.push(targetMenuObj)
+    }
+
+    let total = 0
+    orderedItems.forEach(function(item){
+        total += (item.count * item.price)
+    })
+
+    const orderTotal = document.getElementById('total-price')
+    orderTotal.innerHTML = `$${total}`
+    render()
+}
+
+//currently removing every single item, also returning NaN
+function removeItem(removeThis) {
+    console.log(removeThis)
+    order.innerHTML = order.innerHTML - removeThis
 }
 
 function getMenuData() {
@@ -32,6 +66,22 @@ function getMenuData() {
 
 function render() {
     mainMenu.innerHTML = getMenuData()
+    
+    order.innerHTML = ''
+
+    if (orderedItems){
+        orderedItems.forEach((item) => {
+            order.innerHTML += `
+            <div>
+                <h2>${item.name}</h2>
+                <button id="remove-btn">remove</button>
+                <h4>${item.count}</h4> 
+                <h3>$${item.price}</h3>
+            </div>
+            `
+        })
+
+    }
 }
 
 render()
